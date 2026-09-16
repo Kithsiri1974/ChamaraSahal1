22ecb8d71870673c10bad26061d61d2f54f6e632
Kithsiri1974 <kithsiridaya@gmail.com>
2026-09-16 11:47:25 +0530

    Update stock-mast.js

--- stock-mast.js
+++ stock-mast.js
@@ -124,6 +124,6 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+//app.listen(PORT, () => {
+//  console.log(`Server running at http://localhost:${PORT}`);
+//});
